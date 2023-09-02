d12c9deb2c0457a18c51b463ec96e48053f1c6e9
Curtis Ruck <curtis@ruck.io>
2023-09-02 08:48:00 -0400

    add function env request logging

--- src/routes/api/question/+server.ts
+++ src/routes/api/question/+server.ts
@@ -1,6 +1,7 @@
 import {answerQuestion} from "$lib/apis/chatgpt.server";
 
 export async function POST({request, env}) {
+    console.log(env);
     const { resume, description, question } = await request.json();
 
     return answerQuestion(env.OPENAI_API_KEY, resume, description, question).then((response) => {
