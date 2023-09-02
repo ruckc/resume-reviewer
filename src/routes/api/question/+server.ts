d7f7168c88013c17f18beec63ac2b305db468c79
Curtis Ruck <curtis@ruck.io>
2023-09-02 08:50:36 -0400

    try different  access

--- src/routes/api/question/+server.ts
+++ src/routes/api/question/+server.ts
@@ -1,10 +1,10 @@
 import {answerQuestion} from "$lib/apis/chatgpt.server";
 
-export async function POST({request, env}) {
-    console.log(env);
+export async function POST({request}) {
+
     const { resume, description, question } = await request.json();
 
-    return answerQuestion(env.OPENAI_API_KEY, resume, description, question).then((response) => {
+    return answerQuestion($env.OPENAI_API_KEY, resume, description, question).then((response) => {
         return new Response(JSON.stringify({answer: response}));
     });
 }
